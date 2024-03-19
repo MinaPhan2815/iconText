2060cbc52ed4f090422c21d59343282f9194be69
MinaPhan2815 <minhnhatphan2815@gmail.com>
2024-03-19 11:37:04 +0700

    add google commit

--- auth/login.js
+++ auth/login.js
@@ -29,21 +29,29 @@ const signUp = () => {
 
   firebase.auth().createUserWithEmailAndPassword(email, password)
     .then((result) => {
-      const userId = result.user.uid;
-      result.user.updateProfile({
+      const user = result.user;
+      user.sendEmailVerification().then(function() {
+        console.log('Đã gửi mail xác thực.');
+      }).catch(function(error) {
+        console.error('Có lỗi khi gửi xác thực: ', error);
+      });
+
+      const userId = user.uid;
+      user.updateProfile({
         displayName: fullName
       }).then(() => {
         console.log('ok')
       }).catch((error) => {
         console.log('fail')
       });
+
       firebase.database().ref('users/' + userId).set({
         fullName: fullName,
         email: email
       })
         .then(() => {
-          alert('Đăng ký thành công!');
-          showLoginForm()
+          alert('Đăng ký thành công! Vui lòng kiểm tra email để xác nhận tài khoản.');
+          showLoginForm();
         })
         .catch((error) => {
           console.error("Error saving user data: ", error);
@@ -56,8 +64,8 @@ const signUp = () => {
     });
 }
 
-// Sign In function
 
+// Sign In function
 const signIn = () => {
   const email = document.getElementById("Inemail").value;
   const password = document.getElementById("Inpassword").value;
@@ -65,16 +73,51 @@ const signIn = () => {
 
   firebase.auth().signInWithEmailAndPassword(email, password)
     .then((result) => {
-      console.log("Đăng nhập thành công:", result.user.displayName);
-      localStorage.setItem('displayName', result.user.displayName);
-
-      location.href = "../index.html";
+      const user = result.user;
+      if (user.emailVerified) {
+        console.log(result.user)
+        localStorage.setItem('displayName', user.displayName);
+        location.href = "../index.html";
+      } else {
+        user.sendEmailVerification().then(function() {
+          console.log('Đã gửi lại mail xác thực.');
+        }).catch(function(error) {
+          console.error('Có lỗi khi gửi lại xác thực: ', error);
+        });
+        messageLog.textContent = "Email của bạn chưa được xác nhận. Vui lòng kiểm tra email và xác nhận tài khoản trước khi đăng nhập.";
+        firebase.auth().signOut();
+      }
     })
     .catch((error) => {
-      console.log("Lỗi đăng nhập: ", error.code);
+      console.error("Lỗi khi đăng nhập: ", error.message);
       messageLog.textContent = error.message;
     });
 }
+
+// Sign in with Google
+const signInWithGoogle = () => {
+  try {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    
+    firebase.auth().signInWithPopup(provider)
+      .then((result) => {
+        const user = result.user;
+        
+        location.href = '../index.html'
+        return user;
+      })
+      .catch((error) => {
+        console.error("Lỗi đăng nhập bằng Google:", error);
+        
+        return null;
+      });
+  } catch (error) {
+    console.error("Lỗi đăng nhập bằng Google:", error);
+    
+    return null;
+  }
+}
+
 
 const signOut = () => {
   firebase.auth().signOut()
